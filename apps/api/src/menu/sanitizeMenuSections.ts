697e231832ff0cf92dabedc3a59851902c69be69
fredydomingus22-wq <fredy.domingus22@gmail.com>
2026-01-29 15:36:22 +0100

    Delete apps/api/src/menu/sanitizeMenuSections.ts

--- apps/api/src/menu/sanitizeMenuSections.ts
+++
@@ -1,9 +0,0 @@
-function sanitizeMenuSections(sections: any[]) {
-  return sections.map((s) => ({
-    id: s.id,
-    type: s.type,
-    name: s.name,
-    isActive: s.isActive,
-    config: s.config,
-  }));
-}
